3b6bd92012abaa98703d3184c42f00d655ccd479
Trung Nguyen <trung1704ptit@gmail.com>
2023-03-11 22:20:49 +0800

    check io exists

--- src/controllers/currency.controller.js
+++ src/controllers/currency.controller.js
@@ -1,25 +1,14 @@
-// const httpStatus = require('http-status');
-// const redis = require('redis');
 const catchAsync = require('../utils/catchAsync');
 const { currencyService } = require('../services');
-
-// let redisClient;
-
-// (async () => {
-//   redisClient = redis.createClient();
-
-//   redisClient.on('error', (error) => console.error(`Error : ${error}`));
-
-//   await redisClient.connect();
-// })();
 
 const updateCurrency = catchAsync(async (req, res) => {
   const { currencies } = req.body;
   const io = req.app.get('socketio');
 
   currencies.forEach((currency) => {
-    // redisClient.set(currency.pairName, JSON.stringify(currency));
-    io.emit(currency.pairName, currency);
+    if (io) {
+      io.emit(currency.pairName, currency);
+    }
   });
 
   currencyService.updateCurrencyByBatch(currencies);
@@ -31,7 +20,6 @@ const handleSocketConnect = async (socket) => {
   const { query } = socket.handshake;
   // query in DB
   const cacheCurrency = await currencyService.getCurrencyByPairName(query.pairName);
-  // if currency exists on DB, returns to client.
   if (cacheCurrency) {
     socket.emit(query.pairName, cacheCurrency);
   }
