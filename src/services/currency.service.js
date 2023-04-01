4c73bfdebf8feb70fd0fe94f6b779e86ef3d61a8
Trung Nguyen <trung1704ptit@gmail.com>
2023-04-01 23:12:33 +0800

    refactor code

--- src/services/currency.service.js
+++ src/services/currency.service.js
@@ -1,6 +1,4 @@
-const { CurrencyPair, CurrencyRates } = require('../models');
 const redisClient = require('../redis');
-const catchAsync = require('../utils/catchAsync');
 
 // const cron = require('node-cron');
 
@@ -38,155 +36,123 @@ function round(value, decimals) {
 //   }
 // });
 
-/**
- * Create a currency
- * @param {Object} currencyBody
- * @returns {Promise<Currency>}
- */
-const createCurrency = catchAsync(async currencyBody => {
-  const result = CurrencyPair.create(currencyBody);
-  return result;
-});
-
-// const createCurrencyRates = async (from, currencyList) => {
-//   const result = await CurrencyRates.create({
-//     from,
-//     rates: currencyList,
-//   });
-
-//   return result;
-// };
 
 /**
  * @param {ObjectId} pairName
  * @returns {Promise<Currency>}
  */
-const getCurrencyByPairName = catchAsync(async (pairName) => {
-  let data = null;
-  if (!pairName) return data;
+const getCurrencyByPairName = async (pairName) => {
+  try {
+    let data = null;
+    if (!pairName) return data;
 
-  const spliter = pairName.split('/');
-  const dataCached = await redisClient.get(spliter[0]);
-  if (dataCached) {
-    data = JSON.parse(dataCached);
-    data = data.rates.find(item => item.to === to);
-  }
+    const spliter = pairName.split('/');
+    const dataCached = await redisClient.get(spliter[0]);
+    if (dataCached) {
+      data = JSON.parse(dataCached);
+      data = data.rates.find((item) => item.to === to);
+    }
 
-  return data;
-});
-
-const getCurrencyRatesByFrom = catchAsync(async (from, to) => {
-  if (!from) {
+    return data;
+  } catch (error) {
+    console.log('error in getCurrencyByPairName: ', error);
     return null;
   }
-  let data;
-  const dataCached = await redisClient.get(from);
-  if (dataCached) {
-    data = JSON.parse(dataCached);
-  }
-  
-  // else {
-  //   data = await CurrencyRates.findOne({ from });
-  // }
+};
 
-  if (data) {
-    let rates = data.rates;
-    if (to) {
-      const targetList = to
-        .split(',')
-        .filter((item) => item)
-        .map((item) => item.toUpperCase());
-      rates = data.rates.filter((item) => targetList.includes(item.to.toUpperCase()));
+const getCurrencyRatesByFrom = async (from, to) => {
+  try {
+    if (!from) {
+      return null;
     }
-    return {
-      success: true,
-      from,
-      to,
-      rates,
-    };
-  }
-  return null;
-});
-
-const convertCurrency = catchAsync(async (from, to, amount) => {
-  if (from && to && amount) {
     let data;
     const dataCached = await redisClient.get(from);
     if (dataCached) {
       data = JSON.parse(dataCached);
     }
-    
-    // else {
-    //   data = await CurrencyRates.findOne({ from: from.toUpperCase() });
-    // }
-    if (data) {
-      const targetList = to
-        .split(',')
-        .filter((item) => item)
-        .map((item) => item.toUpperCase());
-      const rates = data.rates.filter((item) => targetList.includes(item.to.toUpperCase()));
-      const converts = rates.map((item) => ({ ...item, amount, result: round(item.price * amount, 6) }));
 
+    if (data) {
+      let rates = data.rates;
+      if (to) {
+        const targetList = to
+          .split(',')
+          .filter((item) => item)
+          .map((item) => item.toUpperCase());
+        rates = data.rates.filter((item) => targetList.includes(item.to.toUpperCase()));
+      }
       return {
         success: true,
         from,
         to,
-        amount,
-        converts,
+        rates,
       };
     }
+    return null;
+  } catch (error) {
+    console.log('Error in getCurrencyRatesByFrom: ', error);
+    return null;
   }
-  return null;
-});
+};
 
-// /**
-//  * @param {ObjectId} pairName
-//  * @param {Object} currencyBody
-//  * @returns {Promise<Currency>}
-//  */
-// const updateCurrencyByPairName = async (pairName, currencyBody) => {
-//   const currency = await getCurrencyByPairName(pairName);
-//   if (!currency) {
-//     return createCurrency(currencyBody);
-//   }
-//   Object.assign(currency, currencyBody);
-//   await currency.save();
-//   return currency;
-// };
+const convertCurrency = async (from, to, amount) => {
+  try {
+    if (from && to && amount) {
+      let data;
+      const dataCached = await redisClient.get(from);
+      if (dataCached) {
+        data = JSON.parse(dataCached);
+      }
 
-// const updateCurrencyRates = async (from, currencyList) => {
-//   const currencyRates = await getCurrencyRatesByFrom(from);
-//   if (!currencyRates) {
-//     return createCurrencyRates(from, currencyList);
-//   }
-//   await CurrencyRates.findOneAndUpdate({ from }, { $set: { rates: currencyList, lastUpdated: new Date() } });
-// };
+      if (data) {
+        const targetList = to
+          .split(',')
+          .filter((item) => item)
+          .map((item) => item.toUpperCase());
+        const rates = data.rates.filter((item) => targetList.includes(item.to.toUpperCase()));
+        const converts = rates.map((item) => ({ ...item, amount, result: round(item.price * amount, 6) }));
+
+        return {
+          success: true,
+          from,
+          to,
+          amount,
+          converts,
+        };
+      }
+    }
+    return null;
+  } catch (error) {
+    console.log('error in convertCurrency: ', error);
+    return null;
+  }
+};
+
 
 /**
  *
  * @param {Object[]} currencies
  */
+const updateCurrencyByBatch = async (currencies) => {
+  try {
+    const groupByCategory = currencies.reduce((group, product) => {
+      const { from } = product;
+      group[from] = group[from] ?? [];
+      group[from].push(product);
+      return group;
+    }, {});
 
-const updateCurrencyByBatch = catchAsync(async (currencies) => {
-  const groupByCategory = currencies.reduce((group, product) => {
-    const { from } = product;
-    group[from] = group[from] ?? [];
-    group[from].push(product);
-    return group;
-  }, {});
-
-  Object.keys(groupByCategory).forEach(async (key) => {
-    const dataCached = { lastUpdated: new Date(), rates: groupByCategory[key] };
-    redisClient.set(key, JSON.stringify(dataCached));
-    // await updateCurrencyRates(key, groupByCategory[key]);
-  });
-});
+    Object.keys(groupByCategory).forEach(async (key) => {
+      const dataCached = { lastUpdated: new Date(), rates: groupByCategory[key] };
+      redisClient.set(key, JSON.stringify(dataCached));
+    });
+  } catch (error) {
+    console.log('error in updateCurrencyByBatch:', error);
+  }
+};
 
 module.exports = {
-  createCurrency,
   convertCurrency,
   getCurrencyByPairName,
-  // updateCurrencyByPairName,
   updateCurrencyByBatch,
   getCurrencyRatesByFrom,
 };
